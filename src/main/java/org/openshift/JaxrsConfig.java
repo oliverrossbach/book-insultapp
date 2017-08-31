d693e50b5f88eaff3b121d487d52dd70390dbbd7
Oliver Rossbach <oliver.rossbach@utinity.com>
2017-08-31 19:37:51 +0200

    Jaxrs

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by orossbach on 31.08.17.
+ */
+
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application {
+}
